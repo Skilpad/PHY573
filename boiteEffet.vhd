4b296e1984836bc24a1897b7a0227f3cb3af1ef5
Matthieu Claybrough <devillcritics@hotmail.fr>
2011-10-11 17:21:19 +0200

    First fixes

--- boiteEffet.vhd
+++ boiteEffet.vhd
@@ -50,128 +50,121 @@ signal b : std_logic := '1';
 signal hasRdLwrR : std_logic := '0';
 signal hasRdRwrL : std_logic := '0';
 
-signal todac  : std_logic_vector (15 downto 0);
-signal fromadc: std_logic_vector (15 downto 0);
-signal rdRwrL : std_logic := '0';
-signal rdLwrR : std_logic := '0';
+signal todac   : std_logic_vector (15 downto 0);
+signal fromadc : std_logic_vector (15 downto 0);
+signal rdRwrL  : std_logic := '0';
+signal rdLwrR  : std_logic := '0';
 
- signal     I2C_SCLK    : std_logic;   -- horloge du bus I²C
-  signal    I2C_SDAT    : std_logic; -- donnée  du bus I²C
-  signal    AUD_DACDAT  : std_logic;   -- DAC donnée audio
- signal     AUD_ADCLRCK : std_logic;   -- ADC horloge Gauche/Droite
- signal     AUD_ADCDAT  : std_logic;    -- ADC donnée audio
- signal     AUD_DACLRCK : std_logic;   -- DAC horloge Gauche/Droite
- signal     AUD_XCK     : std_logic;   -- horloge du codec
-  signal    AUD_BCLK    : std_logic;    -- ADC/DAC horloge bit
+signal I2C_SCLK    : std_logic;   -- horloge du bus I²C
+signal I2C_SDAT    : std_logic; -- donnée  du bus I²C
+signal AUD_DACDAT  : std_logic;   -- DAC donnée audio
+signal AUD_ADCLRCK : std_logic;   -- ADC horloge Gauche/Droite
+signal AUD_ADCDAT  : std_logic;    -- ADC donnée audio
+signal AUD_DACLRCK : std_logic;   -- DAC horloge Gauche/Droite
+signal AUD_XCK     : std_logic;   -- horloge du codec
+signal AUD_BCLK    : std_logic;    -- ADC/DAC horloge bit
 
+signal lastL : std_logic_vector (15 downto 0);
+signal lastR : std_logic_vector (15 downto 0);
 
-  
 
 
 
 begin
 
 
-appui <= b and (a xor b);
+
+	circuit_2 : muxsoninout
+	port map(
+		clk     => CLOCK_50,
+		todac   => todac,
+		fromadc => fromadc,
+		rdRwrL  => rdRwrL,
+		rdLwrR  => rdLwrR,
+		-- signaux 
+		I2C_SCLK    => I2C_SCLK,
+		I2C_SDAT    => I2C_SDAT,
+		AUD_DACDAT  => AUD_DACDAT,
+		AUD_ADCLRCK => AUD_ADCLRCK,
+		AUD_ADCDAT  => AUD_ADCDAT,
+		AUD_DACLRCK => AUD_DACLRCK,
+		AUD_XCK     => AUD_XCK,
+		AUD_BCLK    => AUD_BCLK  
+	);
 
 
-LedsRouges : for toto in 0 to 17 generate
-
-LEDR(toto) <= '1' when (danse = toto) else '0';
-
-end generate;
 	
-
-with tempo select
-	-- RAPPEL: 1 = INACTIF, 0 = ACTIF
-	maxDivis <=10000000 when 1,
-				5000000 when 2,
-				1000000 when 3,
-				500000 when 4,
-				100000 when 5,
-				50000 when 6,
-				10000 when 7,
-				5000 when 8,
-				1000 when 9,
-				100000 when others;
-				
-
- 
-process (CLOCK_50)
+	process (CLOCK_50)
 	begin
-	
-	if CLOCK_50'event and CLOCK_50 = '1' then
 
-		b <= a;
-		a <= KEY(3);
-		
-	end if;
-end process;
- 
- 
-process (CLOCK_50)
-begin
-
- 	if CLOCK_50'event and CLOCK_50 = '1' then
-
-	
-		if rdRwrL = '1' then 
-		-- Lecture droite, ecriture gauche
-			if hasRdRwrL = '0' then
-				todac <= fromadc;
-	  
-				hasRdRwrL <= '1';
+		if CLOCK_50'event and CLOCK_50 = '1' then
+			
+			if rdLwrR = '1' then
+				lastL <= fromadc;
 			end if;
-		else
-		   hasRdRwrL <= '0';
-		end if;
-	  
-	
-	-- todac   : in std_logic_vector (15 downto 0);  -- donnee HP (signed)
-   -- fromadc : out std_logic_vector (15 downto 0); -- donnee ligne (signed)    
-	  
 
-		if hasRdLwrR = '1' then
-			-- Lecture gauche, ecriture droite
-			if hasRdLwrR = '0' then
-				todac <= fromadc;
-				hasRdLwrR <= '1';
+			if rdRwrL = '1' then
+				lastR <= fromadc;
 			end if;
-		else
-			hasRdLwrR <= '0';
+			
 		end if;
-	end if;
-		
+
+	end process;	
+
+
 	
-	                
+	todac <= lastR when rdLwrR = '1' else lastL ;
 
 
-end process;	
 
 
-circuit_2 : muxsoninout
-port map(
-clk     => CLOCK_50,
-todac   => todac,
-fromadc => fromadc,
-rdRwrL  => rdRwrL,
-rdLwrR  => rdLwrR,
--- signaux 
-I2C_SCLK    => I2C_SCLK,
-I2C_SDAT    => I2C_SDAT,
-AUD_DACDAT  => AUD_DACDAT,
-AUD_ADCLRCK => AUD_ADCLRCK,
-AUD_ADCDAT  => AUD_ADCDAT,
-AUD_DACLRCK => AUD_DACLRCK,
-AUD_XCK     => AUD_XCK,
-AUD_BCLK    => AUD_BCLK  
-);
 
-circuit_1 : decodeur
-port map(
-nombre => tempo, -- tempo
-sortie => HEX0
-);
+
+
+	--  ## OLD PORJECT ##
+
+	appui <= b and (a xor b);
+
+
+	LedsRouges : for toto in 0 to 17 generate
+
+	LEDR(toto) <= '1' when (danse = toto) else '0';
+
+	end generate;
+		
+
+	with tempo select
+		-- RAPPEL: 1 = INACTIF, 0 = ACTIF
+		maxDivis <=10000000 when 1,
+					5000000 when 2,
+					1000000 when 3,
+					500000 when 4,
+					100000 when 5,
+					50000 when 6,
+					10000 when 7,
+					5000 when 8,
+					1000 when 9,
+					100000 when others;
+					
+
+	 
+	process (CLOCK_50)
+		begin
+		
+		if CLOCK_50'event and CLOCK_50 = '1' then
+
+			b <= a;
+			a <= KEY(3);
+			
+		end if;
+	end process;
+	 
+	 
+	circuit_1 : decodeur
+	port map(
+		nombre => tempo, -- tempo
+		sortie => HEX0
+	);
 
 
 
